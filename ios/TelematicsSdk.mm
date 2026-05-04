499bf712e69266850509a67347c093123f7fe9ef
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2026-05-04 22:39:04 +0300

    Migrate to React Native new architecture

--- ios/TelematicsSdk.mm
+++ ios/TelematicsSdk.mm
@@ -1,13 +1,16 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#import <ReactCommon/RCTTurboModule.h>
+#import <TelematicsSdkSpec/TelematicsSdkSpec.h>
 
-#if __has_include("RNTelematicsSdkSpec.h")
-#import "RNTelematicsSdkSpec.h"
-#endif
-
-@class TelematicsSdk;
-
-#if __has_include("RNTelematicsSdkSpec.h")
-@interface TelematicsSdk () <NativeTelematicsSdkSpec>
+@interface RCT_EXTERN_MODULE(TelematicsSdk, RCTEventEmitter)
 @end
-#endif
+
+@implementation TelematicsSdk (TurboModule)
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+  return std::make_shared<facebook::react::NativeTelematicsSdkSpecJSI>(params);
+}
+
+@end
